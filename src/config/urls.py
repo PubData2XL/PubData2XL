53ce2e92b38b69c0c5e777245dc0dd41beefdd57
Ivan Rivera <ivanjrivera@proton.me>
2024-05-11 15:30:03 -0600

    Update urls.py

--- src/config/urls.py
+++ src/config/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path("sitemap.xml", TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml")),
     path('', include('apps.pwa.urls')),
-    path('', include('apps.pubmed2xl.urls')),
+    path('', include('apps.pubdata2xl.urls')),
 ]
 
