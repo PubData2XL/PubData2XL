321ae17d2e79f38b128885a747398dab87c80b67
Ivan Rivera <ivanjrivera@proton.me>
2024-05-11 15:09:56 -0600

    Update apps.py

--- src/apps/pubmed2xl/apps.py
+++ src/apps/pubmed2xl/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PubMed2XLConfig(AppConfig):
+class PubData2XLConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.pubmed2xl'
+    name = 'apps.pubdata2xl'
