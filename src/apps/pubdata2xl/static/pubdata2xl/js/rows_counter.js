72e3084af09e1c6cb912118031143f990a010ff7
Ivan Rivera <ivanjrivera@proton.me>
2024-07-02 12:38:33 -0700

    update js

--- src/apps/pubdata2xl/static/pubdata2xl/js/rows_counter.js
+++ src/apps/pubdata2xl/static/pubdata2xl/js/rows_counter.js
@@ -10,16 +10,19 @@ textarea.addEventListener('input', () => {
   textarea.parentNode.dataset.replicatedValue = textarea.value;
   rowsCounter(textarea, counter, link_ids, baseURL_regex);
 })
-
+// Count how many rows (pmids) in textarea.
 function rowsCounter(textarea, counter, link_ids, regex) {
   const text = textarea.value.replace("/",",");
   const lines = text.split("\n");
-  const count = lines.length;
+  const filtered = lines.filter(elm => elm);
+  const count = filtered.length;
   row = " rows"
   if (count <= 1){
     row = " row"
   }
   counter.innerHTML = count + row;
+  // This update the links based on page context.
+  // e.g. if in excel page show XML link, but if in XML page show excel link
   for (let i = 0; i < link_ids.length; i++) {
     // console.log(link_ids[i])
     var anchor = document.getElementById(link_ids[i]);
@@ -27,12 +30,7 @@ function rowsCounter(textarea, counter, link_ids, regex) {
     anchor.setAttribute("href", link + lines);
   }
 }
-
-function ConvertURLS(id, regex, pmids) {
-  
-}
-
-
+// Set blinking caret at the end on load
 document.addEventListener('load', setCaretPosition(textarea, -1, -1));
 function setCaretPosition(ctrl, start, end) {
   // IE >= 9 and other browsers
